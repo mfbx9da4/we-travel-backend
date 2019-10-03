b559ac1243f750f570d5f2e9bd9c1743226a2b90
David Adler <dalberto.adler@gmail.com>
2019-10-03 19:32:45 +0100

    change pq

--- priority-queue.go
+++ priority-queue.go
@@ -20,7 +20,7 @@ func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
 	// We want Pop to give us the lowest
-	return pq[i].Priority < pq[j].Priority
+	return pq[i].Priority > pq[j].Priority
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
